9cbda3c202c629023009c2cd7900727ed73820c8
curious-vv1 <96409335+curious-vv1@users.noreply.github.com>
2022-09-09 23:39:27 +0530

    Create vPattern.cpp

---
+++ vPattern.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  char ch;
+  cin>>ch;
+  int i,j,k;
+  for(i=1;i<=5;i++){
+    for(j=1;j<=i;j++){
+      cout<<j;
+    } 
+    for(k=5;k>=j;k--){
+      cout<<"  ";
+    }
+    for(k=5;k>=j;k--){
+      cout<<"  ";
+    }
+    for(k=1;k<=i;k++)
+    {cout<<j-k;
+      
+    }
+    cout<<"\n";
+  }
+  
+  //write your code here
+  
+  return 0;
+}
