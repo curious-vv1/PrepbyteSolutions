41aaefe1a5ebfecf508cf71447103717615d77ce
curious-vv1 <96409335+curious-vv1@users.noreply.github.com>
2022-10-10 06:03:10 +0530

    Create characterValue.cpp

---
+++ characterValue.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  //write your code here
+  char c;
+  cin>>c;
+  if(c=='p' || c=='P'){
+    cout<<"PrepBytes";
+  }
+  else if(c=='z' || c=='Z'){
+    cout<<"Zenith";
+  }
+  else if(c=='e' || c=='E'){
+    cout<<"Expert Coder";
+  }
+  else if(c=='d' || c=='D'){
+    cout<<"Data Structure";
+  }
+  return 0;
+}
