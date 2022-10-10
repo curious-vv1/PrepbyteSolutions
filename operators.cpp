1ac31ff2411ac284a2f738061f0227a377db19d3
curious-vv1 <96409335+curious-vv1@users.noreply.github.com>
2022-10-10 06:02:34 +0530

    Create operators.cpp

---
+++ operators.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  //write your code here
+  int a,b;
+  cin>>a>>b;
+  if(a>b){
+    cout<<a<<" is greater than "<<b;
+  }
+  else if(a<b){
+    cout<<a<<" is smaller than "<<b;
+  }
+  else{
+    cout<<a<<" is equal to "<<b;
+  }
+  return 0;
+}
