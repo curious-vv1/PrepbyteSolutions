8aa70d6481a5de8cdf89a05830c065fde63a99c3
curious-vv1 <96409335+curious-vv1@users.noreply.github.com>
2022-10-10 06:06:18 +0530

    Create prepbuddyAndSocks.cpp

---
+++ prepbuddyAndSocks.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  //write your code here
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    cout<<n+1<<"\n";
+  }
+  return 0;
+}
